409bc18b562226ccff01f8c09722ccbe02842bc6
Frank Micheal <harlex.mikkey@gmail.com>
2024-07-29 23:49:44 +0100

    setting up react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import "./App.css";
+import {
+   createBrowserRouter,
+   createRoutesFromElements,
+   RouterProvider,
+   Route,
+} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import Homepage from "./pages/Homepage";
+
+const router = createBrowserRouter(
+   createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+         <Route index element={<Homepage />} />
+      </Route>
+   )
+);
 
 function App() {
-   return (
-      <div className="App relative overflow-hidden">Tax return User app</div>
-   );
+   return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,9 @@
+const Homepage = () => {
+   return (
+      <div>
+         <h1 className="text-5xl font-bold underline">Tax return User</h1>
+      </div>
+   );
+};
+
+export default Homepage;
