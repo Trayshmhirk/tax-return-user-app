7f74cca33f4643f252fbc65c02b09d3a42296a9f
Frank Micheal <harlex.mikkey@gmail.com>
2024-08-08 20:23:40 +0100

    knowledge base page, adding video card component, other minor changes

---
+++ src/components/VideoCard.tsx
@@ -0,0 +1,37 @@
+import { MdOutlinePlayCircle } from "react-icons/md";
+import WatchImg from "../assets/watch-small.png";
+
+type VideoCardPropTypes = {
+   title: string;
+   time: string;
+   handleClick: () => void;
+};
+
+const VideoCard = ({ title, time, handleClick }: VideoCardPropTypes) => {
+   const watchStyle = {
+      background: `url(${WatchImg})`,
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover",
+   };
+
+   return (
+      <div
+         onClick={handleClick}
+         className="calc-width-two flex flex-col bg-white dark:bg-gray rounded cursor-pointer shadow-md dark:shadow-md-dark md:w-64"
+      >
+         <div className="relative h-14 text-white md:h-24" style={watchStyle}>
+            <div className="absolute bottom-3 left-6 flex items-center gap-3">
+               <MdOutlinePlayCircle className="w-6 h-6" />
+               <p className="font-medium">Watch</p>
+            </div>
+         </div>
+
+         <div className="px-4 py-3">
+            <p className="text-sm md:text-base">{title}</p>
+            <p className="text-xs md:text-sm">{time}</p>
+         </div>
+      </div>
+   );
+};
+
+export default VideoCard;
--- src/pages/KnowledgeBase.tsx
+++ src/pages/KnowledgeBase.tsx
@@ -1,5 +1,152 @@
+import React, { useState } from "react";
+import SearchAndFilter from "../components/SearchAndFilter";
+import VideoCard from "../components/VideoCard";
+
+type VideoDataTypes = {
+   title: string;
+   time: string;
+   uploaded: string;
+   id: string;
+   category: string;
+};
+
+const videoData: VideoDataTypes[] = [
+   {
+      title: "View previous filing",
+      time: "1mins 15secs",
+      uploaded: "1 day ago",
+      id: "previous-filing",
+      category: "tax",
+   },
+   {
+      title: "Create new filing",
+      time: "2mins 15secs",
+      uploaded: "2 days ago",
+      id: "new-filing",
+      category: "tax",
+   },
+   {
+      title: "Scan receipt",
+      time: "1mins 15secs",
+      uploaded: "4 days ago",
+      id: "scan-receipt",
+      category: "payment",
+   },
+   {
+      title: "Upload e-receipt",
+      time: "2mins 15secs",
+      uploaded: "5 days ago",
+      id: "upload-receipt",
+      category: "payment",
+   },
+   {
+      title: "Share receipt",
+      time: "1mins 15secs",
+      uploaded: "7 days ago",
+      id: "share-receipt",
+      category: "payment",
+   },
+];
+
 const KnowledgeBase = () => {
-   return <div>KnowledgeBase</div>;
+   const [searchInput, setSearchInput] = useState("");
+   const [selectedFilter, setSelectedFilter] = useState("");
+   const [activeFilter, setActiveFilter] = useState("All");
+   const filterTitleList = [
+      "All",
+      "Tax",
+      "Payment",
+      // "Attestation",
+      // "Accounting",
+   ];
+
+   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setSearchInput(e.target.value);
+   };
+
+   const searchVideos = (video: VideoDataTypes) => {
+      const videos = video.category;
+      return videos.toLowerCase().includes(searchInput.toLowerCase());
+   };
+
+   const handleFilter = (title: string) => {
+      setSelectedFilter(title);
+   };
+
+   const filterByVideoCategory = (video: VideoDataTypes) => {
+      if (selectedFilter === "" || selectedFilter === "All") {
+         return true;
+      }
+      return video.category.toLowerCase() === selectedFilter.toLowerCase();
+   };
+
+   const filteredVideos = videoData
+      ? videoData.filter(
+           (video) => searchVideos(video) && filterByVideoCategory(video)
+        )
+      : [];
+
+   const taxFilteredVideos = filteredVideos.filter(
+      (video) => video.category === "tax"
+   );
+
+   const paymentFilteredVideos = filteredVideos.filter(
+      (video) => video.category === "payment"
+   );
+
+   const handleSelectVideo = () => {};
+
+   return (
+      <div className="flex flex-col gap-7">
+         <SearchAndFilter
+            handleSearch={handleSearch}
+            handleFilter={handleFilter}
+            activeFilter={activeFilter}
+            setActiveFilter={setActiveFilter}
+            title={filterTitleList}
+         />
+
+         <div className="flex flex-col gap-3">
+            <p className="font-medium">Tax filing</p>
+            <div className="w-full flex flex-wrap gap-4 justify-between md:justify-normal">
+               {taxFilteredVideos.length ? (
+                  taxFilteredVideos.map((video, index) => (
+                     <VideoCard
+                        key={index}
+                        title={video.title}
+                        time={video.time}
+                        handleClick={() => handleSelectVideo()}
+                     />
+                  ))
+               ) : (
+                  <p className="w-full pending-text text-center">
+                     No results found
+                  </p>
+               )}
+            </div>
+         </div>
+
+         <div className="flex flex-col gap-3">
+            <p className="font-medium">Store receipts</p>
+            <div className="w-full flex flex-wrap gap-4 justify-between md:justify-normal">
+               {paymentFilteredVideos.length ? (
+                  paymentFilteredVideos.map((video, index) => (
+                     <VideoCard
+                        key={index}
+                        title={video.title}
+                        time={video.time}
+                        handleClick={() => handleSelectVideo()}
+                     />
+                  ))
+               ) : (
+                  <p className="w-full pending-text text-center">
+                     No results found
+                  </p>
+               )}
+            </div>
+         </div>
+      </div>
+   );
 };
 
 export default KnowledgeBase;
