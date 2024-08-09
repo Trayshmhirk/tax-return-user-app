62c4047ec0abd5f123de8c845649039e7d131f19
Frank Micheal <harlex.mikkey@gmail.com>
2024-08-09 01:34:09 +0100

    minor changes

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -4,7 +4,7 @@ const TodoCard = ({ todoIcon: TodoIcon, text, handleClick }: TodoCardProps) => {
    return (
       <div
          onClick={handleClick}
-         className="calc-width-two h-32 flex flex-col justify-center items-center gap-2 bg-bubbles dark:bg-gray text-richElectricBlue dark:text-white px-3 rounded-lg cursor-pointer shadow-md dark:shadow-md-dark hover-shadow-body"
+         className="calc-width-two h-32 flex flex-col justify-center items-center gap-2 bg-bubbles dark:bg-gray text-richElectricBlue dark:text-white px-3 rounded-lg cursor-pointer shadow-md dark:shadow-md-dark hover-shadow-body md:calc-width-four"
       >
          <TodoIcon className="w-6 h-6" />
          <p className="text-center">{text}</p>
