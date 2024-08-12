3f9b1556c209b69a70b37cc9361ed6bfb6ce1de2
Frank Micheal <harlex.mikkey@gmail.com>
2024-08-12 21:58:24 +0100

    adding video data file, completing the video page.

--- src/pages/KnowledgeBase.tsx
+++ src/pages/KnowledgeBase.tsx
@@ -2,52 +2,7 @@ import React, { useState } from "react";
 import SearchAndFilter from "../components/SearchAndFilter";
 import VideoCard from "../components/VideoCard";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
-
-type VideoDataTypes = {
-   title: string;
-   time: string;
-   uploaded: string;
-   id: string;
-   category: string;
-};
-
-const videoData: VideoDataTypes[] = [
-   {
-      title: "View previous filing",
-      time: "1mins 15secs",
-      uploaded: "1 day ago",
-      id: "ad3f79ja7HD985af",
-      category: "tax",
-   },
-   {
-      title: "Create new filing",
-      time: "2mins 15secs",
-      uploaded: "2 days ago",
-      id: "7fdYbfj73748ffgb",
-      category: "tax",
-   },
-   {
-      title: "Scan receipt",
-      time: "1mins 15secs",
-      uploaded: "4 days ago",
-      id: "vnFHu830847bjhbf",
-      category: "payment",
-   },
-   {
-      title: "Upload e-receipt",
-      time: "2mins 15secs",
-      uploaded: "5 days ago",
-      id: "Pfb7bYB8fbY47cb",
-      category: "payment",
-   },
-   {
-      title: "Share receipt",
-      time: "1mins 15secs",
-      uploaded: "7 days ago",
-      id: "0bfbd74BvdtGff8bf9j",
-      category: "payment",
-   },
-];
+import { videoData, VideoDataTypes } from "../mocks/VideoData";
 
 const KnowledgeBase = () => {
    const navigate = useNavigate();
--- src/pages/Video.tsx
+++ src/pages/Video.tsx
@@ -1,9 +1,113 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { videoData } from "../mocks/VideoData";
+import WatchImg from "../assets/watch-large.png";
+import { MdOutlinePlayCircle } from "react-icons/md";
+import VideoCard from "../components/VideoCard";
 
 const Video = () => {
+   const navigate = useNavigate();
    const { videoId } = useParams();
 
-   return <div>Video {videoId}</div>;
+   const watchStyle = {
+      background: `url(${WatchImg})`,
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover",
+   };
+
+   // Find the selected video based on the videoId
+   const selectedVideo = videoData.find((video) => video.id === videoId);
+
+   // Find sibling videos in the same category
+   const siblingVideos = videoData.filter(
+      (video) => video.category === selectedVideo?.category
+   );
+
+   // Find the videoData that matches the videoID
+   // videoData.forEach((category) => {
+   //    const video = category.videos.find((video) => video.id === videoId);
+   //    if (video) {
+   //       selectedVideo = video;
+   //       parentTitle = category.category;
+   //       siblingVideo = category.videos;
+   //    }
+   // });
+
+   const handleSelectVideo = (videoId: string) => {
+      // Navigate to the "Watch Video" route and pass the title as a parameter
+      navigate(`/knowledge-base/video/${videoId}`);
+   };
+
+   return (
+      <div className="flex flex-col gap-9">
+         {" "}
+         <div className="w-full flex flex-col self-center gap-4 md:w-[600px] lg:w-[800px] xl:w-[1000px]">
+            <h3 className="text-xl font-medium">
+               {<p>{selectedVideo?.title}</p>}
+            </h3>
+
+            <div className="flex flex-col gap-2">
+               <div
+                  className="relative h-40 w-full flex justify-center items-center rounded-lg cursor-pointer md:h-56"
+                  style={watchStyle}
+               >
+                  <div className="flex items-center gap-2">
+                     <div className="flex justify-center items-center p-1">
+                        <MdOutlinePlayCircle className="w-6 h-6" />
+                     </div>
+                     <span className="font-bold">Watch full video</span>
+                  </div>
+               </div>
+               <p className="text-sm text-mutedGray dark:text-white">
+                  {selectedVideo?.uploaded}
+               </p>
+            </div>
+
+            <div className="flex flex-col gap-2">
+               <h5 className="font-medium text-lg">Brief description</h5>
+               <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Corporis quos dicta quasi praesentium, natus, sapiente labore
+                  accusantium, nesciunt nostrum reprehenderit doloribus nihil
+                  iusto inventore porro repellendus. Earum soluta veniam dolorum
+                  fuga, minus aperiam debitis possimus animi alias assumenda
+                  sint vero aspernatur ratione blanditiis eius. Reiciendis
+                  libero voluptatum architecto, fugiat provident accusantium
+                  deleniti quas, sapiente nam ab quia. Cum id facere quibusdam
+                  reiciendis culpa possimus consectetur quis. Eligendi quidem
+                  fugit mollitia cum alias aliquam sit hic, laborum, officia,
+                  numquam fugiat vel at! Esse quaerat unde dignissimos
+                  voluptates non. Vero id, magni veniam animi fuga odio neque
+                  esse deleniti praesentium? Necessitatibus dicta repudiandae ex
+                  aut distinctio voluptatum quis quibusdam temporibus
+                  repellendus culpa inventore ut fugiat minus velit rem,
+                  laboriosam rerum incidunt delectus deserunt exercitationem.
+                  Odit illo atque laborum distinctio in dolorum adipisci at
+                  aspernatur vero rerum. Nostrum, consectetur nobis. Provident
+                  perferendis nemo iusto, ab eveniet eos cumque, sequi debitis
+                  atque dolore sunt?
+               </p>
+            </div>
+
+            <div className="flex flex-col gap-4 mb-4">
+               <h4 className="font-medium">Coming up</h4>
+               <div className="flex flex-wrap gap-3">
+                  {siblingVideos.map((video) =>
+                     video.id !== videoId ? (
+                        <VideoCard
+                           key={video.id}
+                           title={video.title}
+                           time={video.time}
+                           handleClick={() => handleSelectVideo(video.id)}
+                        />
+                     ) : (
+                        <></>
+                     )
+                  )}
+               </div>
+            </div>
+         </div>
+      </div>
+   );
 };
 
 export default Video;
