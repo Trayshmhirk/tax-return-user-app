157c458bc50a63a47f7c7fb2aab2883f52bb4770
Frank Micheal <harlex.mikkey@gmail.com>
2024-08-17 00:37:10 +0100

    Add form mobile view

--- src/layouts/FormLayout.tsx
+++ src/layouts/FormLayout.tsx
@@ -4,8 +4,8 @@ import FormImage from "../assets/forms-images.png";
 const FormLayout = () => {
    return (
       <div className="w-full min-h-screen bg-ghostWhite dark:bg-eerieBlack sm:flex sm:justify-center sm:items-center">
-         <div className="flex justify-between w-[900px] h-[650px] bg-[#E9E9E9] p-1 rounded-xl shadow-lg">
-            <div className="w-[500px] overflow-hidden">
+         <div className="w-full h-full flex justify-between md:w-[800px] md:h-[620px] lg:w-[900px] lg:h-[650px] bg-[#E9E9E9] sm:p-1 rounded-xl shadow-lg">
+            <div className="hidden w-[500px] overflow-hidden md:block">
                <img src={FormImage} className="h-full object-contain" />
             </div>
             <Outlet />
