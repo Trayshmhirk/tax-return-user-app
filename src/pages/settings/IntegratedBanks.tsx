64ea449c44c24842ca240e15d0700c53af2cdc2b
Frank Micheal <harlex.mikkey@gmail.com>
2024-09-03 02:53:56 +0100

    delete redundant file

--- src/pages/settings/IntegratedBanks.tsx
+++
@@ -1,9 +0,0 @@
-const IntegratedBanks = () => {
-   return (
-      <div className="h-[600px] flex flex-col gap-7 mt-4 px-2 py-3 overflow-scroll">
-         <h1 className="text-lg font-semibold">Integrated banks</h1>
-      </div>
-   );
-};
-
-export default IntegratedBanks;
