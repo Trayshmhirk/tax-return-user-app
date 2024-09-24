85e048fc6af2b52bbebbbdc61c679be829f86956
Frank Micheal <harlex.mikkey@gmail.com>
2024-09-24 23:45:07 +0100

    factoring chats ui

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,7 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
 import { truncateString } from "@/helpers/truncateString";
 import { IoChatbubbles } from "react-icons/io5";
+import React, { useState } from "react";
 
 type Chats = {
    title: string;
@@ -9,7 +12,18 @@ type Chats = {
    id: string;
 };
 
+type ChatPropType = {
+   title: string;
+   conversations: [];
+};
+
+type ConversationsPropTypes = {
+   date: string;
+};
+
 const LiveChat = () => {
+   const [activeChat] = useState(null);
+
    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
       e;
    };
@@ -46,6 +60,36 @@ const LiveChat = () => {
          id: "",
       },
    ];
+
+   const chat: ChatPropType[] = [
+      {
+         title: "",
+         conversations: [],
+      },
+   ];
+   const conversations: ConversationsPropTypes[] = [
+      {
+         date: "",
+      },
+   ];
+
+   const isChatAccess = "off";
+
+   // Sort function for dates
+   const sortDates = (dateA: string, dateB: string) => {
+      const dateOrder = ["yesterday", "today"];
+
+      const indexA = dateOrder.indexOf(dateA.toLowerCase());
+      const indexB = dateOrder.indexOf(dateB.toLowerCase());
+
+      if (indexA === -1 && indexB === -1) {
+         // If both dates are not in the predefined order, compare them as normal dates
+         // return new Date(dateA) - new Date(dateB);
+      }
+
+      // If one of the dates is not in the predefined order, prioritize it
+      return indexA - indexB;
+   };
 
    return (
       <div className="w-full h-full flex">
@@ -112,9 +156,86 @@ const LiveChat = () => {
                </aside>
 
                <div className="hidden md:flex w-full h-full flex-col items-center justify-center border border-spanishGray rounded-md">
-                  <p className="pending-text">
-                     Click on a chat to begin conversation
-                  </p>
+                  {chat.length ? (
+                     <>
+                        {chat.map((chat, index) => (
+                           <React.Fragment>
+                              <div className="px-5 py-2">{chat.title}</div>
+                              <div className="chat-body overflow-scroll w-100 d-flex flex-column pt-4 px-4 gap-4">
+                                 {activeChat &&
+                                    Object.entries(
+                                       conversations[activeChat] ?? {}
+                                    )
+                                       .sort(([dateA], [dateB]) =>
+                                          sortDates(dateA, dateB)
+                                       )
+                                       .map(([date /*messages*/]) => (
+                                          <React.Fragment key={date}>
+                                             <div className="d-flex align-items-center gap-3">
+                                                {/* <Line isChat /> */}
+                                                <p className="w-25 text-center">
+                                                   {date}
+                                                </p>
+                                                {/* <Line isChat /> */}
+                                             </div>
+
+                                             <div key={index} className="">
+                                                {/* <ChatMessages
+                                                   messages={messages}
+                                                /> */}
+                                             </div>
+                                          </React.Fragment>
+                                       ))}
+                                 {/* <div className='chat-notifications d-flex align-items-center align-self-center p-3'>
+                                                <NotificationMessage 
+                                                   statusIcon={CheckIcon}
+                                                   notificationText='Your payment service task has been completed'
+                                                   notificationAction='Pay now'
+                                                   isChatNotification
+                                                   handleNotificationAction={handleShowInvoiceModal}
+                                                />
+                                             </div> */}
+                              </div>
+
+                              {isChatAccess === "off" ? (
+                                 <div className="chat-disabled d-flex justify-content-center text-center">
+                                    This chat has been disabled by the admin.
+                                    Kindly check back later.
+                                 </div>
+                              ) : (
+                                 <div className="chat-foot d-flex align-items-center gap-3 rounded-1">
+                                    <img
+                                    // src={AttachFileIcon}
+                                    // onClick={handleInsertFileModal}
+                                    />
+
+                                    <div className="text w-100 d-flex justify-content-between rounded-2">
+                                       <input
+                                          className="w-75"
+                                          type="text"
+                                          // placeholder="Type your message"
+                                          // value={inputMessage}
+                                          // onChange={handleInputChange}
+                                          // onKeyDown={handleKeyPress}
+                                       />
+
+                                       <img
+                                       // src={SendIcon}
+                                       // onClick={handleSend}
+                                       />
+                                    </div>
+                                 </div>
+                              )}
+                           </React.Fragment>
+                        ))}
+                     </>
+                  ) : (
+                     <div className="chat-box w-100 h-100 d-flex align-items-center justify-content-center">
+                        <p className="pending-text">
+                           Click on a chat to begin conversation
+                        </p>
+                     </div>
+                  )}
                </div>
             </div>
          </div>
