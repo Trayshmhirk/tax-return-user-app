f54e4a967818362c4b2091771b9eaaf92b289c29
Frank Micheal <harlex.mikkey@gmail.com>
2024-10-18 17:19:04 +0100

    delete old hook file

--- src/hooks/UseSidebar.tsx
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { SidebarProviderContext } from "@/contexts/SidebarContext";
-
-export const useSidebar = () => {
-   return useContext(SidebarProviderContext);
-};
