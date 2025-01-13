89019ba968e8fe1ec10741748d0e993ccf770059
Frank Micheal <harlex.mikkey@gmail.com>
2025-01-13 20:12:08 +0100

    install and initialize sonner toaster

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,25 @@ import { ErrorBoundary } from "react-error-boundary";
 import { Router } from "@/routes/Router";
 import ContextProvider from "@/provider/ContextProvider";
 import { ErrorPage } from "@/pages/ErrorPage";
+import { Toaster } from "sonner";
 
 function App() {
-   return (
-      <div className="App min-h-screen flex content-center">
-         <ErrorBoundary FallbackComponent={ErrorPage}>
-            <ContextProvider>
-               <Router />
-            </ContextProvider>
-         </ErrorBoundary>
-      </div>
-   );
+  return (
+    <div className="App min-h-screen flex content-center">
+      <ErrorBoundary FallbackComponent={ErrorPage}>
+        <ContextProvider>
+          <Toaster
+            expand={true}
+            richColors
+            visibleToasts={3}
+            gap={14}
+            position="top-right"
+          />
+          <Router />
+        </ContextProvider>
+      </ErrorBoundary>
+    </div>
+  );
 }
 
 export default App;
